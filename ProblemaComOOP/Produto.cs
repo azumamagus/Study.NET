0dfd56b4b77b7e7cff0c3b064500602069c900ee
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2021-06-04 16:42:44 -0300

    Add class produto

---
+++ ProblemaComOOP/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace ProblemaComOOP
+{
+    class Produto
+    {
+        public string Nome; public double Preco; public int Quantidade;
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
