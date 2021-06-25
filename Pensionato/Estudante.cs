2511b501a50f9e1384fb17cfe664884b4c02df69
Bruno da Cunha Felipe <bruno.dc.felipe@gmail.com>
2021-06-25 17:43:32 -0300

    add exercicio

---
+++ Pensionato/Estudante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensionato
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public Estudante(string nome, string email)
+        {
+            Nome = nome; Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
